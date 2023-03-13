83d09c23072fe052e58c24788ec86c85da7a856b
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-14 00:27:00 +0100

    added login and signup nav link

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ProductsView from '../views/ProductsView.vue';
+import LoginView from '../views/LoginView.vue';
+import SignupView from '../views/SignupView.vue';
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/products',
     name: 'products',
     component: ProductsView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView,
   },
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+      <h1>This is the Login page.</h1>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'LoginView',
+    components: {
+    }
+  }
+  </script>
+  
