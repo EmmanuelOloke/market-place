bbce8dedd4d558c971be0e4082cf69354e24750a
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-14 23:53:50 +0100

    show a list of all products using SingleProduct component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="products">
+        <div v-for="product in products" :key="product.id">
+            <SingleProduct :product="product"/>
+        </div>
+    </div>
+    <div v-else>
+        Loading...
+    </div>
+</template>
+
+<script>
+    import SingleProduct from './SingleProduct.vue'
+
+    export default {
+        props: ['products'],
+        components: {
+            SingleProduct
+        }
+    }
+</script>
+
+<style>
+
+</style>
