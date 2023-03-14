ca6555ff80cc8b0e0347d0a62c1b22a163cbaf29
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-14 23:50:31 +0100

    utilized getProducts composable to fetch products from API

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="">
-    <h1>This is the product page</h1>
+    <h1>Shop from our list of products</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="products">
+      <ProductList :products="products.products"/>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
+
+<script>
+  import ProductList from '@/components/ProductList.vue';
+  import getProducts from '@/composables/getProducts';
+
+  export default {
+    name: 'ProductView',
+    components: {
+      ProductList
+    },
+
+    setup() {
+      const {products, error, load} = getProducts()
+
+      load()
+
+      return { products, error }
+    }
+  }
+</script>
+
+<style>
+
+</style>
