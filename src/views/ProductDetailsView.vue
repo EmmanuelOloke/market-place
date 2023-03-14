a2942a8e849c23f26346a0d87ad8a47e65047548
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-14 23:51:07 +0100

    utilized getProduct composable to fetch single product from API

---
+++ src/views/ProductDetailsView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="error">
+        {{ error }}
+    </div>
+
+    <div v-if="product">
+        <h3>{{ product.title }}</h3>
+        <p>{{ product.description }}</p>
+    </div>
+
+    <div v-else>
+        Loading...
+    </div>
+</template>
+
+<script>
+    import getProduct from '@/composables/getProduct'
+    export default {
+        props: ['id'],
+        setup(props) {
+            const {product, error, load} = getProduct(props.id)
+
+            load()
+
+            console.log(product)
+            console.log(props.id)
+
+            return { product, error }
+        }
+    }
+</script>
