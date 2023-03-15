13ee30d847a120c24c122da50a1c6159a722c15b
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-15 23:58:46 +0100

    added signup logic with firebase and google auth provider

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,15 +1,53 @@
 <template>
-    <div class="home">
-      <h1>This is the signup page.</h1>
+    <div class="signup-container">
+      <h1>This is the signup page....</h1>
+      <div>
+        <input type="text" placeholder="Email" v-model="email">
+      </div>
+
+      <div>
+        <input type="password" placeholder="Password" v-model="password">
+      </div>
+
+      <button @click="signup">Sign Up</button>
+      <button @click="signInWithGoogle">Sign in With Google</button>
     </div>
   </template>
   
-  <script>
-  
-  export default {
-    name: 'SignupView',
-    components: {
+  <script setup>
+    import { ref } from 'vue'
+    import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+    import { useRouter } from 'vue-router';
+
+    const email = ref("")
+    const password = ref("")
+    const router = useRouter()
+
+    const signup = () => {
+      const auth = getAuth()
+      createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
+          console.log("Successfully Registered")
+          console.log(auth.currentUser)
+          router.push('/products')
+        })
+        .catch((error) => {
+          console.log(error.code)
+          alert(error.message)
+        })
     }
-  }
+
+    const signInWithGoogle = () => {
+      const provider = new GoogleAuthProvider()
+      signInWithPopup(getAuth(), provider)
+        .then((result) => {
+          console.log(result.user)
+          router.push("/products")
+        })
+        .catch((error) => {
+          console.log(error.message)
+        })
+    }
+
   </script>
   
