da2222752d77f180d275ce942d09760937fb2212
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-16 02:46:27 +0100

    updated styling

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="signup-container">
-      <h1>This is the signup page....</h1>
+      <p>Sign up for Market Place</p>
       <div>
         <input type="text" placeholder="Email" v-model="email">
       </div>
@@ -9,8 +9,11 @@
         <input type="password" placeholder="Password" v-model="password">
       </div>
 
-      <button @click="signup">Sign Up</button>
-      <button @click="signInWithGoogle">Sign in With Google</button>
+      <button class="login-button" @click="signup">Sign Up</button>
+      <button class="google-signup" @click="signInWithGoogle">
+        <img class="logo" src="../assets/google-icon.png" alt="google icon"/>
+        Sign Up With Google
+      </button>
     </div>
   </template>
   
@@ -50,4 +53,42 @@
     }
 
   </script>
+
+  <style>
+    .signup-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 100px auto;
+      width: 300px;
+      padding: 40px 30px 60px;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+      border-radius: 5px;
+    }
+
+    .google-signup {
+      margin-top: 20px;
+      border: none;
+      font-weight: bold;
+      font-size: 1em;
+      width: 17em;
+      padding: 0.3em 0;
+      border-radius: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+      color: #8e9a9f;
+    }
+
+    .google-signup:hover {
+      cursor: pointer;
+      background: #e3f3fa;
+    }
+
+    .logo {
+      width: 40px;
+      height: 40px;
+    }
+  </style>
   
