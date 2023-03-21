780fbbe2fbf6cc42b7aab9781b1d680cf7d3da22
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-21 14:39:13 +0100

    removed unnecessary height on home Hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -70,7 +70,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 600px;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;;
     margin-top: 3em;
     width: 83%;
